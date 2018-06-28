1aa887567bfd5d4e929671ac4473b4591e12b2fd
Scott <scott@Scotts-MacBook-Pro.local>
2018-06-28 19:25:45 -0300

    version-2

--- sudoku/sudoku/main.c
+++ sudoku/sudoku/main.c
@@ -8,21 +8,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+
+int sudoku[9][9];
 
 /* structure for passing data to threads */
-
 typedef struct{
     int row;
     int column;
 } parameters;
 
+void* column_check(void* arg){
+    
+    parameters *param = (parameters*) arg;
+    parameters data = *param;
+    
+    printf("%d", data.row);
+    
+    return 0;
+}
+
 int main(int argc, const char * argv[]) {
    
     // Get the file and save to file variable
-    FILE *file = fopen("/Users/scott/Desktop/sudoku/sudoku/sudokuTestFile.txt", "r");
+    FILE *file = fopen("/Users/scott/Desktop/sudokuCode/sudoku/sudoku/sudokuTestFile.txt", "r");
     
     char input;
-    int sudoku[9][9];
     int i = 0;
     int j = 0;
     
@@ -53,11 +64,17 @@ int main(int argc, const char * argv[]) {
         printf("\n");
     }
     
+    //Parameter data to check columns and rows for the sudoku grid
     parameters *data = (parameters *) malloc(sizeof(parameters));
     data->row = 1;
     data->column = 1;
     
+    pthread_t tid;
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
     
+    pthread_create(&tid, &attr, column_check, data);
+    pthread_join(tid, NULL);
 
     return 0;
 }
